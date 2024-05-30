70409225b60125e598a609211260b44e35c63199
gaofei <wggfox@foxmail.com>
2024-05-30 22:34:19 +0800

    2
    Signed-off-by: gaofei <wggfox@foxmail.com>

--- main.py
+++ main.py
@@ -19,16 +19,6 @@ def kill_process(process_name):
         print(result.stderr)
 
 
-def main_run(browser_name):
-    config.set('browser', 'name', browser_name)
-    config.set('browser', 'headless', 'true')
-    with open(config_path, 'w', encoding='utf-8') as f:
-        config.write(f)
-    pytest.main(
-        ['-sv', '-n=2', '--alluredir', './allure-results-' + browser_name, '--clean-alluredir'])
-    kill_process(f'{browser_name}*')
-
-
 def open_edge_report():
     os.system(f'allure generate ./allure-results-msedge/ -o ./allure-report-msedge/ --clean')
     os.system('allure open --port 10086 allure-report-msedge')
@@ -46,9 +36,15 @@ def open_report_as_serve(features: list):
             executor.submit(feature)
 
 
+def main_run(browser_name):
+    config.set('browser', 'name', browser_name)
+    config.set('browser', 'headless', 'true1')
+    with open(config_path, 'w', encoding='utf-8') as f:
+        config.write(f)
+    pytest.main(
+        ['-sv', '-n=2', '--alluredir', './allure-results-' + browser_name, '--clean-alluredir'])
+    kill_process(f'{browser_name}*')
+
+
 if __name__ == '__main__':
-    # main_run('msedge')
-    # main_run('chrome')
-    # open_report_as_serve([open_edge_report, open_chrome_report])
     main_run('chrome')
-    open_report_as_serve([open_chrome_report])
